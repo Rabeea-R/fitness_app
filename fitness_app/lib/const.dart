dc1f10458f550fec4f50afe2bf8b8b6c994c08bc
Rabeea-R <rabeeanafees16@gmail.com>
2021-10-01 09:31:56 +0500

    pages

--- fitness_app/lib/const.dart
+++ fitness_app/lib/const.dart
@@ -1,3 +1,5 @@
 const kt = 'Train like 2x Pro Fitness Model World Champion Williams Falade';
 const kt1 = 'More Pain';
 const kt2 = 'Get ready for real results with the new age Mr Motivator...';
+const kt3 = 'CHOOSE YOUR PLAN';
+const kt4 = 'Join Today';
--- fitness_app/lib/plans.dart
+++ fitness_app/lib/plans.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/const.dart';
 import 'package:flutter/material.dart';
 
 class Plans extends StatefulWidget {
@@ -15,13 +16,61 @@ class _PlansState extends State<Plans> {
       appBar: AppBar(
         title: Center(
             child: Image.asset('images/plan_logo.png', fit: BoxFit.cover)),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.blueGrey[900],
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
             onPressed: () {},
           ),
         ],
+      ),
+      body: Container(
+        color: Colors.blueGrey[900],
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Text(
+                kt3,
+                style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: new Text(
+                kt4,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ListView(
+  padding: const EdgeInsets.all(8),
+  children: <Widget>[
+    Container(
+      height: 50,
+      color: Colors.amber[600],
+      child: const Center(child: Text('Entry A')),
+    ),
+    Container(
+      height: 50,
+      color: Colors.amber[500],
+      child: const Center(child: Text('Entry B')),
+    ),
+    Container(
+      height: 50,
+      color: Colors.amber[100],
+      child: const Center(child: Text('Entry C')),
+    ),
+  ],
+)
+          ],
+        ),
       ),
     );
   }
